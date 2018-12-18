9eb6b03492ad05bdc06df8e7efc68409b5804c33
Christopher Culp <Christopher.D.Culp@gmail.com>
2018-12-18 11:27:08 -0500

    Started API development for BeyondCampaign

---
+++ BeyondCampaign.API/Models/User.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+
+namespace BeyondCampaign.API.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public string Email { get; set; }
+        public bool? isGM { get; set; }
+        public bool? isPlayer { get; set; }
+        
+
+        
+
+    }
+}
