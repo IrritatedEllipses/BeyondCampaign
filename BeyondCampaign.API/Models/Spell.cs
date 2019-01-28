6dcf70b6d68ba9e2e1c14a875a33c4cf16745c9f
Christopher Culp <Christopher.D.Culp@gmail.com>
2019-01-28 16:28:35 -0500

    Spell models

---
+++ BeyondCampaign.API/Models/Spell.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeyondCampaign.API.Models
+{
+    public class Spell
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Level { get; set; }
+        public string School { get; set; }
+        public string CastingTime { get; set; }
+        public string Range { get; set; }
+        public SpellComponents Components { get; set; }
+        public string Duraction { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ BeyondCampaign.API/Models/SpellComponents.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BeyondCampaign.API.Models
+{
+    public class SpellComponents
+    {
+        public int Id { get; set; }
+        public bool Verbal { get; set; }
+        public bool Somatic { get; set; }
+        public bool Material { get; set; }
+        public int? MaterialCost { get; set; }
+        public string MaterialDescription { get; set; }
+    }
+}
