97d14221e7fa89d4fb6e78e60adebfd82aebab10
Christopher Culp <Christopher.D.Culp@gmail.com>
2019-02-10 20:01:17 -0500

    Model binding for register function

--- beyondcampaign-spa/src/app/register-component/register-component.component.ts
+++ beyondcampaign-spa/src/app/register-component/register-component.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../_services/auth.service';
+import { FormBuilder, FormGroup, Validators, } from '@angular/forms';
+import { User } from '../_models/User';
 
 @Component({
   selector: 'app-register-component',
@@ -6,10 +9,26 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register-component.component.css']
 })
 export class RegisterComponentComponent implements OnInit {
+  user: User;
+  registerForm: FormGroup;
 
-  constructor() { }
+
+  constructor(private authService: AuthService, private fb: FormBuilder, ) { }
 
   ngOnInit() {
   }
 
+  createRegisterForm() {
+    this.registerForm = this.fb.group({
+      userName: ['', Validators.required],
+      email: ['', Validators.required],
+      dateCreated: [null, Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ['', Validators.required],
+    }, {validator: this.passwordMatchValidator});
+  }
+
+  passwordMatchValidator(g: FormGroup) {
+    return g.get('password').value === g.get('confirmPassword').value ? null : {'mismatch': true};
+  }
 }
